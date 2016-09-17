e1c324e9b84d1c893709022e8eadea4d8622c256
carlosmori94 <carlosmori34@gmail.com>
2016-09-17 19:02:45 -0300

    Hasta Post

---
+++ Estacionamiento.php
@@ -0,0 +1,7 @@
+<?php
+$patente = $_POST["Patente"];
+$accion = $_POST["Estacionar"];
+echo "Mi patente tiene el valor de : ".$patente."<BR>";
+echo "y he seleccionado ".$accion;
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="Estacionamiento.php" method="POST">
+		<table>
+			<tr><td colspan="2" align="center">Estacionamiento</td></tr>
+			<tr><td>Ingrese su patente:</td><td><input type="text" name="Patente"></td></tr>
+			<td align="right"><input type="submit" name="Estacionar" value="Ingreso" ></td>
+			<td align="left"><input type="submit" name="Estacionar" value="Egreso"></td>
+			
+		</table>
+	</form>
+</body>
+</html>
